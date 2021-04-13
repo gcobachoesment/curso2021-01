06fcc8605b6239489e94dd06dcd9c7d71c69a12c
Gregorio Cobacho <gcobacho@esment.org>
2021-04-13 16:52:44 +0000

    ADD helpdesk module

---
+++ helpdesk_gcobacho/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Helpdesk Gcobacho",
+    "summary": "Helpdesk test gcobacho",
+    "version": "14.0.1.0.0",
+    "author": "<gcobacho>, Odoo Community Association (OCA)",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [
+        #"security/helpdesk_gcobacho_security.xml",
+        #"security/ir.model.access.csv",
+        "views/helpdesk_menu_view.xml",
+    ],
+}
